afed469864051026924e1809ec0f36f3a290afd4
RECturtle <52131753+RECturtle@users.noreply.github.com>
2020-05-07 06:55:51 -0500

    Updated daily_news.py

--- daily_news.py
+++ daily_news.py
@@ -1,3 +1,9 @@
+#/usr/bin/python3
+
+# Created by Spencer Mertes
+# https://twitter.com/MertleTrtl3
+# 5/1/2020
+
 from siteSources.grab_source import *
 
 getMicrosoft("https://www.microsoft.com/security/blog/")
